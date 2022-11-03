3d85d2fe0d74faa99f48e26cd20488670d2b95ee
kaemon8686 <keamon8686@gmail.com>
2022-11-03 15:44:41 +0200

    modify index

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
-<?php
+<html>
+ <head>
+    <title>Home page</title>
+ </head>
+ <body>
+<h1>Contact form</h1>
 
-echo 'Hello1 ';
+<form action="action.php" method="POST">
 
-?>
+    <label for="name">Name</label>
+    <input id="name" name="name" type="text" placeholder="Name">
+    <input id="email" name="email"  type="email" placeholder="Email">
+    <input id="password1" name="password1" type="password" placeholder="Password">
+
+    <button id="send">OK</button>
+
+</form>
+
+</body>    
+</html>
